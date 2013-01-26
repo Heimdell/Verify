aa08a3499eb4140be2dc0cc93ac557899be40cf0
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-01-26 13:31:38 +0400

    Add Verify.assumptions class method that executes Loader

--- lib/Verify.rb
+++ lib/Verify.rb
@@ -2,5 +2,9 @@ require "Verify/version"
 
 module Verify
   autoload :Assumption, 'Verify/assumption'
+  autoload :Loader,     'Verify/loader'
 
+  def self.assumptions(directory = 'assumptions')
+    Loader.run directory
+  end
 end
